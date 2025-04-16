3744bfe75e94abf4ae75d97b6bf4d5e322cc6bb1
shiflux <harshtamrakar474@gmail.com>
2025-04-16 14:33:47 +0530

    used Express MiddleWare

--- build/graphql/index.js
+++ build/graphql/index.js
@@ -16,32 +16,40 @@ exports.InitGraphqlServer = void 0;
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 const server_1 = require("@apollo/server");
+const express4_1 = require("@apollo/server/express4");
 const express_1 = __importDefault(require("express"));
-const standalone_1 = require("@apollo/server/standalone");
-const body_parser_1 = require("body-parser");
+const cors_1 = __importDefault(require("cors"));
+const body_parser_1 = __importDefault(require("body-parser"));
 const transactions_1 = require("./transactions");
 const budget_1 = require("./budget");
 const InitGraphqlServer = () => __awaiter(void 0, void 0, void 0, function* () {
-    const app = (0, express_1.default)();
     const PORT = Number(process.env.PORT) || 8000;
-    app.use((0, body_parser_1.json)());
     const gqlserver = new server_1.ApolloServer({
         typeDefs: `
-        ${transactions_1.transaction.typedefs}
-        ${transactions_1.transaction.mutations}
-        ${transactions_1.transaction.queries}
-        ${budget_1.budget.typedefs}
-        ${budget_1.budget.mutations}
-        ${budget_1.budget.queries}
-`,
+      ${transactions_1.transaction.typedefs}
+      ${transactions_1.transaction.mutations}
+      ${transactions_1.transaction.queries}
+      ${budget_1.budget.typedefs}
+      ${budget_1.budget.mutations}
+      ${budget_1.budget.queries}
+    `,
         resolvers: {
             Query: Object.assign(Object.assign({}, transactions_1.transaction.resolvers.Query), budget_1.budget.resolvers.Query),
-            Mutation: Object.assign(Object.assign({}, transactions_1.transaction.resolvers.Mutation), budget_1.budget.resolvers.Mutation)
+            Mutation: Object.assign(Object.assign({}, transactions_1.transaction.resolvers.Mutation), budget_1.budget.resolvers.Mutation),
         },
     });
-    const { url } = yield (0, standalone_1.startStandaloneServer)(gqlserver, {
-        listen: { port: PORT },
+    yield gqlserver.start();
+    const app = (0, express_1.default)();
+    // Configure CORS
+    app.use((0, cors_1.default)({
+        origin: ["https://clientfinancetracker.vercel.app"],
+    }));
+    // Add body parser middleware
+    app.use(body_parser_1.default.json());
+    // Attach Apollo Server middleware
+    app.use("/graphql", (0, express4_1.expressMiddleware)(gqlserver));
+    app.listen(PORT, () => {
+        console.log(`🚀 Server ready at http://localhost:${PORT}/graphql`);
     });
-    return url;
 });
 exports.InitGraphqlServer = InitGraphqlServer;
--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -2,43 +2,55 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
 import express from "express";
-import { startStandaloneServer } from "@apollo/server/standalone";
-import { json } from "body-parser";
+import cors from "cors";
+import bodyParser from "body-parser";
 import { transaction } from "./transactions";
 import { budget } from "./budget";
-import cors from "cors";
 
 export const InitGraphqlServer = async () => {
-    const app = express(); 
-    const PORT = Number(process.env.PORT) || 8000; 
-    app.use(json()); 
-    app.use(cors());
-      
-    const gqlserver = new ApolloServer({
-        typeDefs: `
-        ${transaction.typedefs}
-        ${transaction.mutations}
-        ${transaction.queries}
-        ${budget.typedefs}
-        ${budget.mutations}
-        ${budget.queries}
-`,      
-        resolvers: {
-            Query: {
-                ...transaction.resolvers.Query,
-                ...budget.resolvers.Query
-            },
-            Mutation: {
-                ...transaction.resolvers.Mutation, 
-                ...budget.resolvers.Mutation 
-            }
-        }, 
-    });
+  const PORT = Number(process.env.PORT) || 8000;
 
-    const { url } = await startStandaloneServer(gqlserver, {
-        listen: { port: PORT},
-      });
+  const gqlserver = new ApolloServer({
+    typeDefs: `
+      ${transaction.typedefs}
+      ${transaction.mutations}
+      ${transaction.queries}
+      ${budget.typedefs}
+      ${budget.mutations}
+      ${budget.queries}
+    `,
+    resolvers: {
+      Query: {
+        ...transaction.resolvers.Query,
+        ...budget.resolvers.Query,
+      },
+      Mutation: {
+        ...transaction.resolvers.Mutation,
+        ...budget.resolvers.Mutation,
+      },
+    },
+  });
 
-    return url; 
+  await gqlserver.start();
+
+  const app = express();
+
+  // Configure CORS
+  app.use(
+    cors({
+      origin: ["https://clientfinancetracker.vercel.app"],
+    })
+  );
+
+  // Add body parser middleware
+  app.use(bodyParser.json());
+
+  // Attach Apollo Server middleware
+  app.use("/graphql", expressMiddleware(gqlserver));
+
+  app.listen(PORT, () => {
+    console.log(`🚀 Server ready at http://localhost:${PORT}/graphql`);
+  });
 };
